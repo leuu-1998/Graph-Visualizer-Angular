34f432d0fe5d2b337d40c08a11e59dbd43083129
leuu-1998 <lmoinah@uni.pe>
2023-07-19 14:29:56 -0500

    decouple functions from graph component to service

--- src/app/visualizer/services/updategraph.service.ts
+++ src/app/visualizer/services/updategraph.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Graph } from '../types/types';
+import { CreateEdgePayload, CreateNodePayload, Graph, UpdateEdgePayload, UpdateNodePayload } from '../types/types';
 
 @Injectable({
   providedIn: 'root'
@@ -8,41 +8,73 @@ export class UpdategraphService {
 
   constructor() { }
 
-  dataGraphReducer(state: Graph, action: { name: string, payload: any }) {
-    switch (action.name) {
-      case 'create-node':
-        return {
-          ...state,
-          nodesGraph: { ...state.nodesGraph, [state.topNode]: action.payload.node },
-          topNode: state.topNode + 1,
-        };
-      case 'update-node':
-        return {
-          ...state,
-          nodesGraph: { ...state.nodesGraph, [action.payload.id]: action.payload.node }
-        };
-      case 'delete-node':
-        const { nodesGraph, ...restGraphWithoutNodes } = state;
-        const { [action.payload]: removedNode, ...restNodes } = nodesGraph;
-        return { nodesGraph: restNodes, ...restGraphWithoutNodes };
-      case 'create-edge':
-        return {
-          ...state,
-          edgesGraph: { ...state.edgesGraph, [state.topEdge]: action.payload.edge },
-          topEdge: state.topEdge + 1
-        };
-      case 'update-edge':
-        return { ...state, edgesGraph: { ...state.edgesGraph, [action.payload.id]: { ...state.edgesGraph[action.payload.id], w: action.payload.w } } };
-      case 'delete-edge':
-        const { edgesGraph, ...restGraphWithoutEdges } = state;
-        const { [action.payload]: removedEdge, ...restEdges } = edgesGraph;
-        return { edgesGraph: restEdges, ...restGraphWithoutEdges };
-      case 'update-isWeight':
-        return { ...state, isWeighted: !state.isWeighted };
-      case 'update-isDirected':
-        return { ...state, isDirected: !state.isDirected };
-      default:
-        return state;
+  createNode(state: Graph, payload: CreateNodePayload): Graph {
+    return {
+      ...state,
+      nodesGraph: { ...state.nodesGraph, [state.topNode]: payload.node },
+      topNode: state.topNode + 1,
+    };
+  }
+
+  updateNode(state: Graph, payload: UpdateNodePayload): Graph {
+    return {
+      ...state,
+      nodesGraph: { ...state.nodesGraph, [payload.id]: payload.node }
+    };
+  }
+
+  deleteNode(state: Graph, id: string): Graph {
+    Object.entries(state.edgesGraph).forEach((element) => {
+      const [key, edge] = element;
+      if (edge.u == +id || edge.v == +id) {
+        state = this.deleteEdge(state, key);
+      }
+    });
+
+    const { nodesGraph, ...restGraphWithoutNodes } = state;
+    const { [id]: removedNode, ...restNodes } = nodesGraph;
+    return { nodesGraph: restNodes, ...restGraphWithoutNodes };
+  }
+
+  createEdge(state: Graph, payload: CreateEdgePayload): Graph {
+    return {
+      ...state,
+      edgesGraph: { ...state.edgesGraph, [state.topEdge]: payload.edge },
+      topEdge: state.topEdge + 1
+    };
+  }
+
+  updateEdge(state: Graph, payload: UpdateEdgePayload): Graph {
+    return {
+      ...state,
+      edgesGraph: {
+        ...state.edgesGraph,
+        [payload.id]: {
+          ...state.edgesGraph[payload.id],
+          w: payload.w
+        }
+      }
+    };
+  }
+
+  deleteEdge(state: Graph, id: string): Graph {
+    const { edgesGraph, ...restGraphWithoutEdges } = state;
+    const { [id]: removedEdge, ...restEdges } = edgesGraph;
+    return { edgesGraph: restEdges, ...restGraphWithoutEdges };
+  }
+
+  updateIsWeighted(state: Graph) {
+    return { ...state, isWeighted: !state.isWeighted };
+  }
+
+  updateIsDirected(state: Graph) {
+    return { ...state, isDirected: !state.isDirected };
+  }
+
+  findEdge(state: Graph, u: number, v: number) {
+    if (Object.values(state.edgesGraph).find((edge) => edge.u === u && edge.v === v) === undefined) {
+      return false;
     }
+    return true;
   }
 }
